9b3bf8392c2786b1f4c91e1d40bf0f490d69778c
anna <18yk04@gmail.com>
2025-12-22 15:44:22 +0900

    script change

--- deploy-scripts/stop_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Stopping backend app..."
-PID_FILE="/home/ec2-user/backend/app.pid"
-if [ -f $PID_FILE ]; then
-  kill -9 $(cat $PID_FILE)
-  rm -f $PID_FILE
-  echo "Backend stopped"
-else
-  echo "No backend app running"
