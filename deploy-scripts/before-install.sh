12e7d7bda142cda0a9ab9c985d2e2587a270436c
anna <18yk04@gmail.com>
2025-12-23 13:34:42 +0900

    appspec

--- deploy-scripts/before-install.sh
+++ deploy-scripts/before-install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+set -e
+
+# Deploy-scripts 복사 후 권한 적용
 chmod +x /home/ec2-user/deploy-scripts/*.sh
 
-set -e
+# Frontend 초기화
 echo "Cleaning /var/www/frontend"
 rm -rf /var/www/frontend/*
 mkdir -p /var/www/frontend
+sudo chown -R ec2-user:ec2-user /var/www/frontend
